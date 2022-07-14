8818a7b792841c15d5f46ecc3af5e2f235c09d1d
Mashilheiba <gdragonnm@gmail.com>
2022-07-14 09:18:52 +0530

    add ray sensor

--- car.js
+++ car.js
@@ -10,12 +10,14 @@ class Car {
         this.maxSpeed=3;
         this.friction=0.05;
         this.angle=0;
-
+        
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
     update(){
         this.#move();
+        this.sensor.update();
     }
 
     #move(){
@@ -69,5 +71,7 @@ class Car {
         );
         ctx.fill();
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
