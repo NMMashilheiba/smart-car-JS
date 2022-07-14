327a20eabdb730bd2e39f47466b548669317f371
Mashilheiba <gdragonnm@gmail.com>
2022-07-14 17:39:01 +0530

    add simulated traffic

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-    constructor(x, y, width, height){
+    constructor(x, y, width, height,controlType,maxSpeed=3){
         this.x=x;
         this.y=y;
         this.width=width;
@@ -7,30 +7,40 @@ class Car {
 
         this.speed=0;
         this.acceleration=0.2;
-        this.maxSpeed=5;
+        this.maxSpeed=maxSpeed;
         this.friction=0.05;
         this.angle=0;
         this.damaged=false;
         
-        this.sensor = new Sensor(this);
-        this.controls = new Controls();
+        if(controlType!="DUMMY"){
+            this.sensor = new Sensor(this);
+        }
+        this.controls = new Controls(controlType);
     }
 
-    update(roadBorders){
+    update(roadBorders, traffic){
         if(!this.damaged){
             this.#move();
             this.polygon=this.#createPolygon();
-            this.damaged=this.#assessDamage(roadBorders);
+            this.damaged=this.#assessDamage(roadBorders,traffic);
         }
-        this.sensor.update(roadBorders);
+        if(this.sensor){
+            this.sensor.update(roadBorders, traffic);
+        }
     }
 
-    #assessDamage(roadBorders){
+    #assessDamage(roadBorders, traffic){
         for(let i=0; i<roadBorders.length; i++){
             if(polyIntersect(this.polygon, roadBorders[i])){
                 return true;
             }
         }
+        for(let i=0; i<traffic.length; i++){
+            if(polyIntersect(this.polygon, traffic[i].polygon)){
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -94,7 +104,7 @@ class Car {
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx){
+    draw(ctx, color){
         // ctx.save();
         // ctx.translate(this.x,this.y);
         // ctx.rotate(-this.angle);
@@ -112,7 +122,7 @@ class Car {
         if(this.damaged){
             ctx.fillStyle='gray';
         }else{
-            ctx.fillStyle="black";
+            ctx.fillStyle=color;
         }
 
         ctx.beginPath();
@@ -122,6 +132,8 @@ class Car {
         }
         ctx.fill();
 
-        this.sensor.draw(ctx);
+        if(this.sensor){
+            this.sensor.draw(ctx);
+        }
     }
 }
